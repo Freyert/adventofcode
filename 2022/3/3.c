f08b4cec47ae9129db66d98796737c0676e2ae63
Fulton Byrne <fulton.byrne@commercetools.com>
2022-12-22 10:38:32 -0500

    total file length

--- 2022/3/3.c
+++ 2022/3/3.c
@@ -1,6 +1,24 @@
-// a-z 1 to 26 (ascii 97-122)
-// A-Z 27 to 52 (ascii 65-90)
-// 0000 0000 0000 0000 0000 0000 0000 0000
-// ------------------- -------------------
-// need a mapping from value to index in 64 bits
+#include <stdio.h>
+#include <unistd.h>
 
+int main() {
+    FILE* input = fopen("./input.txt", "r");
+    char buff[50];
+    int length = 0;
+    //read the full buffer each time.
+    while(1) {
+     //https://stackoverflow.com/questions/3167298/how-can-i-convert-a-file-pointer-file-fp-to-a-file-descriptor-int-fd
+     ssize_t bytesRead = read(fileno(input), buff, sizeof(buff));
+
+     if (bytesRead <= 0) {
+        break;
+     }
+
+     ssize_t i = 0;
+     while (i < bytesRead) {
+        length += 1;
+        i += 1;
+     }
+    }
+    printf("%d bytes\n", length);
+}
